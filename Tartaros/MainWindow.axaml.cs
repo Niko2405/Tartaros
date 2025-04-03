bc9e9e71ca3e78482eb2c1c814792ba06a47ae87
Niko2405 <34937693+Niko2405@users.noreply.github.com>
2025-04-03 19:24:10 +0200

    Create project

---
+++ Tartaros/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Tartaros
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Tartaros/Program.cs
+++ Tartaros/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace Tartaros
+﻿using System;
+using Avalonia;
+
+namespace Tartaros
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
 
-			Console.WriteLine("Hello, World!");
-        }
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .WithInterFont()
+                .LogToTrace();
     }
 }
