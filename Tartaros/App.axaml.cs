46cda2302fa17c50eb144fb9ca77850047ccdc6c
Niko2405 <34937693+Niko2405@users.noreply.github.com>
2025-04-03 20:53:05 +0200

    Test Login Window

--- Tartaros/App.axaml.cs
+++ Tartaros/App.axaml.cs
@@ -15,7 +15,8 @@ namespace Tartaros
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow();
+                //desktop.MainWindow = new MainWindow();
+                desktop.MainWindow = new LoginWindow();
             }
 
             base.OnFrameworkInitializationCompleted();
