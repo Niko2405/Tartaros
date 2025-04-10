9752f8e07a0dc005ad18f6155ada310a2ae71422
Niko2405 <34937693+Niko2405@users.noreply.github.com>
2025-04-10 20:12:07 +0200

    add server / client instance

--- Tartaros/Program.cs
+++ Tartaros/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Tartaros
+﻿using TLogger;
+
+namespace Tartaros
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.WriteLine($"TLogger Version: {Logger.VERSION}");
+			Init.Start();
+			Thread.Sleep(2500);
+			foreach (var arg in args)
+			{
+				if (arg == "--client")
+				{
+					Console.Title = "Tartaros - Client";
+					Client.Active = true;
+					Server.Active = false;
+				}
+				if (arg == "--server")
+				{
+					Console.Title = "Tartaros - Server";
+					Client.Active = false;
+					Server.Active = true;
+				}
+			}
+			if (Server.Active)
+			{
+				// TODO: start service
+				// TODO: Connect to hardware COM
+				// TODO: start cli
+				Server.RunCommandLine();
+			}
+			if (Client.Active)
+			{
+				// TODO: login to server
+			}
 		}
 	}
 }
---
+++ Tartaros/Terminal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TLogger;
+
+namespace Tartaros
+{
+	internal class Terminal
+	{
+		public static readonly string LOGO = "\r\n _____          _                      \r\n|_   _|_ _ _ __| |_ __ _ _ __ ___  ___ \r\n  | |/ _` | '__| __/ _` | '__/ _ \\/ __|\r\n  | | (_| | |  | || (_| | | | (_) \\__ \\\r\n  |_|\\__,_|_|   \\__\\__,_|_|  \\___/|___/\r\n\r\n";
+	}
+}
